2460a8245f47d37231211c45ca12f633a15b2d83
Petrus Jarl <petrus@jarl.nu>
2025-11-01 19:10:57 +0100

    Add click block to filter

--- components/SemesterBlock.tsx
+++ components/SemesterBlock.tsx
@@ -1,10 +1,12 @@
 import { COURSES } from "@/app/courses";
 import semestersAtom from "@/app/atoms/semestersAtom";
-import { useAtomValue } from "jotai";
-import { Plus } from "lucide-react";
+import { useAtomValue, useSetAtom } from "jotai";
+import { SearchIcon } from "lucide-react";
 import { CourseCard } from "@/components/CourseCard/Card";
 import { Draggable } from "@/components/CourseCard/Draggable";
 import { Droppable } from "./Dropable";
+import { produce } from "immer";
+import { filterAtom } from "@/app/atoms/FilterAtom";
 
 type SemesterBlockProps = {
     semesterNumber: number;
@@ -15,13 +17,14 @@ type SemesterBlockProps = {
 
 export const SemesterBlock = ({ semesterNumber, periodNumber, blockNumber }: SemesterBlockProps) => {
     const semesters = useAtomValue(semestersAtom);
+    const setFilter = useSetAtom(filterAtom);
     const courseCode: string | null = semesters[semesterNumber][periodNumber][blockNumber];
     const course = courseCode ? COURSES[courseCode] : null;
     if (!courseCode || !course) {
         return (
             <Droppable key={blockNumber} id={`semester-${semesterNumber}-period${periodNumber}-block-${blockNumber}`} data={{ semester: semesterNumber, period: periodNumber, block: blockNumber }}>
                 <div className="flex flex-col items-center">
-                    <Plus className="text-zinc-500 hover:text-foreground size-16"/>
+                    <SearchIcon className="text-zinc-500 hover:text-foreground size-12" onClick={onClickFilter}/>
                     <span className="text-zinc-500">Block {blockNumber + 1}</span>
                 </div>
             </Droppable>
@@ -35,4 +38,23 @@ export const SemesterBlock = ({ semesterNumber, periodNumber, blockNumber }: Sem
 
         </Droppable>
     )
+
+    function onClickFilter() {
+        unCheckOtherTimeSlots();
+        setFilter(produce((draft) => {
+            draft.semester[semesterNumber] = true;
+            draft.period[periodNumber] = true;
+            draft.block[blockNumber] = true;
+        }
+    ))
+    }
+    function unCheckOtherTimeSlots() {
+        setFilter(produce((draft) => {
+            draft.semester = draft.semester.map(() => false);
+            draft.period = draft.period.map(() => false);
+            draft.block = draft.block.map(() => false);
+        }
+    ))
+    }
+
 }
