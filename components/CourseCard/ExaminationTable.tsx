bd7bec561e37ccf1e01c8a0553e844aa3d312544
Petrus Jarl <petrus@jarl.nu>
2026-01-13 20:40:15 +0100

    Add examination table

--- components/CourseCard/ExaminationTable.tsx
+++ components/CourseCard/ExaminationTable.tsx
@@ -1,13 +1,38 @@
 import { CourseExamination } from "@/app/(main)/page";
 import { FC } from "react";
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "../ui/table";
+import { Scale } from "@/prisma/generated/client/enums";
+import { Label } from "@radix-ui/react-label";
 
 type ExaminationTableProps = {
-    examination: CourseExamination;
+    examination: CourseExamination[];
 };
-    
+
+
+
 const ExaminationTable: FC<ExaminationTableProps> = ({ examination }) => {
     return (
-        "HEJ"
+        
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Module</TableHead>
+                    <TableHead>Credits</TableHead>
+                    <TableHead>Scale</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {examination.map((exam) => (
+                    <TableRow key={exam.module}>
+                        <TableCell>{exam.name}</TableCell>
+                        <TableCell>{exam.module}</TableCell>
+                        <TableCell>{exam.credits} ECTS</TableCell>
+                        <TableCell>{exam.scale == Scale.G_OR_U ? "U, G" : "U, 3, 4, 5"}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     );
 };
 
