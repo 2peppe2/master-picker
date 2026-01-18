4a30d349cd5f2fd959bdf62034c683dd5ea299cc
Mikael <32036218+BakuPlayz@users.noreply.github.com>
2026-01-18 12:17:22 +0100

    Fix only include master credits in master requirements bar (#37)

--- app/(main)/(mastersRequirementsBar)/MastersRequirementsBar.tsx
+++ app/(main)/(mastersRequirementsBar)/MastersRequirementsBar.tsx
@@ -1,15 +1,15 @@
-import { Card, CardTitle } from "@/components/ui/card";
-
 import { MastersBadgeRequirementTooltip } from "./components/MastersBadgeRequirementTooltip";
 import { useEvaluateMasterProgress } from "./hooks/useEvaluateMasterProgress";
+import { FC, ReactNode, useEffect, useState } from "react";
 import { MasterBadge } from "@/components/MasterBadge";
+import { Card, CardTitle } from "@/components/ui/card";
 import {
   getMastersWithRequirements,
   MastersWithRequirements,
 } from "@/app/actions/getMasters";
-import { useEffect, useState } from "react";
+import { range } from "lodash";
 
-export const MastersRequirementsBar = () => {
+const MastersRequirementsBar = () => {
   const [mastersWithRequirements, setMastersWithRequirements] =
     useState<MastersWithRequirements | null>(null);
   const evaluateMasterProgress = useEvaluateMasterProgress();
@@ -19,35 +19,60 @@ export const MastersRequirementsBar = () => {
   }, []);
 
   if (!mastersWithRequirements) {
-    return null;
+    return <MastersRequirementsSkeleton />;
   }
 
   return (
-    <Card className="p-4 mb-4">
-      <CardTitle className="flex gap-4 text-lg">
-        Master Requirements:
-        {mastersWithRequirements.map((master) => {
-          const requirements = master.requirements.flatMap(
-            (req) => req.requirements
-          );
-          const { fulfilled, progress } = evaluateMasterProgress(requirements);
-          return (
-            <MasterBadge
-              name={master.master}
-              key={master.master}
-              text={`${progress}%`}
-              tooltip={({ masterName }) => (
-                <MastersBadgeRequirementTooltip
-                  name={masterName}
-                  master={master.master}
-                  fulfilled={fulfilled}
-                  all={requirements}
-                />
-              )}
-            />
-          );
-        })}
-      </CardTitle>
-    </Card>
+    <MastersRequirementsContainer>
+      {mastersWithRequirements.map((master) => {
+        const requirements = master.requirements.flatMap(
+          (req) => req.requirements,
+        );
+        const { fulfilled, progress } = evaluateMasterProgress(requirements);
+
+        return (
+          <MasterBadge
+            name={master.master}
+            key={master.master}
+            text={`${progress}%`}
+            tooltip={({ masterName }) => (
+              <MastersBadgeRequirementTooltip
+                name={masterName}
+                master={master.master}
+                fulfilled={fulfilled}
+                all={requirements}
+              />
+            )}
+          />
+        );
+      })}
+    </MastersRequirementsContainer>
   );
 };
+
+const MastersRequirementsSkeleton = () => (
+  <MastersRequirementsContainer>
+    {range(0, 8).map((i) => (
+      <div
+        key={`master-sekeleton-${i}`}
+        className="h-6 w-[50px] bg-muted animate-pulse rounded-full"
+      />
+    ))}
+  </MastersRequirementsContainer>
+);
+interface MastersRequirementsContainerProps {
+  children: ReactNode;
+}
+
+const MastersRequirementsContainer: FC<MastersRequirementsContainerProps> = ({
+  children,
+}) => (
+  <Card className="p-4 mb-4">
+    <CardTitle className={`flex flex-wrap gap-4 text-lg items-center`}>
+      Master Requirements:
+      {children}
+    </CardTitle>
+  </Card>
+);
+
+export default MastersRequirementsBar;
