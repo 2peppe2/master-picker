092e7ade1effe2105da2172c34caa273e4a5bdab
Petrus Jarl <petrus@jarl.nu>
2026-02-04 21:09:31 +0100

    Finish landing

--- app/(landing)/LandingClientPage.tsx
+++ app/(landing)/LandingClientPage.tsx
@@ -9,7 +9,7 @@ import {
   ComboboxList,
 } from "@/components/ui/combobox";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 interface LandingClientPageProps {
   programs: {
@@ -19,42 +19,76 @@ interface LandingClientPageProps {
     programCourses: {
       startYear: number;
     }[];
+    masters: {
+      master: string;
+      name: string;
+    }[];
   }[];
 }
 
 const LandingClientPage = ({ programs }: LandingClientPageProps) => {
   const [selectedProgram, setSelectedProgram] = useState<string | null>(null);
   const [selectedYear, setSelectedYear] = useState<string | null>(null);
+  const [selectedMaster, setSelectedMaster] = useState<string | null>(null);
   const router = useRouter();
 
-  const handleGetStarted = () => {
+  const pushToGuide = () => {
+    if (!selectedProgram || !selectedYear || !selectedMaster) return;
+    const params = new URLSearchParams({
+      program: selectedProgram,
+      year: selectedYear,
+      master: selectedMaster,
+    });
+    router.push(`/guide?${params.toString()}`);
+  };
+  const pushToDashboard = () => {
     if (!selectedProgram || !selectedYear) return;
     const params = new URLSearchParams({
       program: selectedProgram,
       year: selectedYear,
     });
-    router.push(`/guide?${params.toString()}`);
-  };
+    router.push(`/dashboard?${params.toString()}`);
+  }
+
+  const programItems = useMemo(
+    () =>
+      programs.map((p) => ({
+        label: `${p.shortname} - ${p.name}`,
+        value: p.program,
+      })),
+    [programs],
+  );
+  const yearItems = useMemo(() => {
+    if (!selectedProgram) return [];
+    const program = programs.find((p) => p.program === selectedProgram);
+    if (!program) return [];
+    return program.programCourses.map((c) => c.startYear.toString());
+  }, [programs, selectedProgram]);
+
+  const masterItems = useMemo(() => {
+    if (!selectedProgram) return [];
+    const program = programs.find((p) => p.program === selectedProgram);
+    if (!program) return [];
+    return program.masters.map((m) => ({
+      label: m.name,
+      value: m.master,
+    }));
+  }, [programs, selectedProgram]);
+
   return (
     <div className="min-h-screen ">
       <main className="flex flex-col items-center justify-center text-center px-4 pt-32 pb-20">
-        <h1
-          className="text-2xl md:text-6xl font-bold"
-          style={{ fontFamily: '"DM Serif Text", serif' }}
-        >
-          LiU Master
-        </h1>
-        <p className="mb-8 max-w-xl text-center text-lg text-muted-foreground">
-          Drop the crazy excel sheets.
-          <br />
-          Embrace effortless course planning.
-        </p>
-
+        <Header />
         <div className="flex flex-col items-center gap-8">
           <Combobox
-            items={programs.map((p) => p.program)}
-            value={selectedProgram}
-            onValueChange={setSelectedProgram}
+            items={programItems}
+            value={
+              programItems.find((item) => item.value === selectedProgram) || {
+                label: "",
+                value: "",
+              }
+            }
+            onValueChange={(item) => setSelectedProgram(item?.value || null)}
           >
             <ComboboxInput
               placeholder="Select your current program"
@@ -63,85 +97,100 @@ const LandingClientPage = ({ programs }: LandingClientPageProps) => {
             <ComboboxContent>
               <ComboboxEmpty>No programs found.</ComboboxEmpty>
               <ComboboxList>
-                {(item) => {
-                  const program = programs.find((p) => p.program === item);
-                  const label = program
-                    ? `${program.shortname} - ${program.name}`
-                    : item;
-                  return (
-                    <ComboboxItem key={item} value={item}>
-                      {label}
+                {(item) => (
+                  <ComboboxItem key={item.value} value={item}>
+                    {item.label}
+                  </ComboboxItem>
+                )}
+              </ComboboxList>
+            </ComboboxContent>
+          </Combobox>
+
+          <Combobox
+            items={yearItems}
+            value={selectedYear}
+            onValueChange={setSelectedYear}
+          >
+            <ComboboxInput
+              disabled={selectedProgram === null}
+              placeholder="Select starting year"
+              aria-hidden={!selectedProgram}
+              className={`w-80 h-12 [&_[data-slot=input-group-control]]:text-base md:[&_[data-slot=input-group-control]]:text-lg [&_[data-slot=input-group-control]]:px-4 transition-[opacity,transform] duration-200 ease-in-out ${
+                selectedProgram
+                  ? "opacity-100 translate-y-0 pointer-events-auto"
+                  : "opacity-0 -translate-y-1 pointer-events-none"
+              }`}
+            />
+
+            <ComboboxContent>
+              <ComboboxEmpty>Year not found.</ComboboxEmpty>
+              <ComboboxList>
+                {(item) => (
+                  <ComboboxItem key={item} value={item}>
+                    {item}
+                  </ComboboxItem>
+                )}
+              </ComboboxList>
+            </ComboboxContent>
+          </Combobox>
+          <div
+            className={`text-sm transition-[opacity,transform] duration-200 ease-in-out ${
+              selectedYear
+                ? "opacity-100 translate-y-0 pointer-events-auto"
+                : "opacity-0 -translate-y-1 pointer-events-none"
+            }`}
+          >
+            <Combobox
+              items={masterItems}
+              value={
+                masterItems.find((item) => item.value === selectedMaster) || {
+                  label: "",
+                  value: "",
+                }
+              }
+              onValueChange={(item) => setSelectedMaster(item?.value || null)}
+            >
+              <ComboboxInput
+                disabled={selectedYear === null}
+                placeholder="Select desired master"
+                className={`w-80 h-12 [&_[data-slot=input-group-control]]:text-base md:[&_[data-slot=input-group-control]]:text-lg [&_[data-slot=input-group-control]]:px-4`}
+              />
+
+              <ComboboxContent>
+                <ComboboxEmpty>Master not found.</ComboboxEmpty>
+                <ComboboxList>
+                  {(item) => (
+                    <ComboboxItem key={item.value} value={item}>
+                      {item.label}
                     </ComboboxItem>
-                  );
-                }}
-              </ComboboxList>
-            </ComboboxContent>
-          </Combobox>
+                  )}
+                </ComboboxList>
+              </ComboboxContent>
+            </Combobox>
 
-          <Combobox
-            items={
-              selectedProgram
-                ? (programs
-                    .find((p) => p.program === selectedProgram)
-                    ?.programCourses.map((c) => c.startYear.toString()) ?? [])
-                : []
-            }
-            value={selectedYear}
-            onValueChange={setSelectedYear}
-          >
-            <ComboboxInput
-              disabled={selectedProgram === null}
-              placeholder="Select starting year"
-              className="w-80 h-12 [&_[data-slot=input-group-control]]:text-base md:[&_[data-slot=input-group-control]]:text-lg [&_[data-slot=input-group-control]]:px-4"
-            />
-
-            <ComboboxContent>
-              <ComboboxEmpty>Year not found.</ComboboxEmpty>
-              <ComboboxList>
-                {(item) => (
-                  <ComboboxItem key={item} value={item}>
-                    {item}
-                  </ComboboxItem>
-                )}
-              </ComboboxList>
-            </ComboboxContent>
-          </Combobox>
-
-          <Combobox
-            items={
-              selectedProgram
-                ? (programs
-                    .find((p) => p.program === selectedProgram)
-                    ?.programCourses.map((c) => c.startYear.toString()) ?? [])
-                : []
-            }
-            value={selectedYear}
-            onValueChange={setSelectedYear}
-          >
-            <ComboboxInput
-              disabled={selectedProgram === null}
-              placeholder="Select starting year"
-              className="w-80 h-12 [&_[data-slot=input-group-control]]:text-base md:[&_[data-slot=input-group-control]]:text-lg [&_[data-slot=input-group-control]]:px-4"
-            />
-
-            <ComboboxContent>
-              <ComboboxEmpty>Year not found.</ComboboxEmpty>
-              <ComboboxList>
-                {(item) => (
-                  <ComboboxItem key={item} value={item}>
-                    {item}
-                  </ComboboxItem>
-                )}
-              </ComboboxList>
-            </ComboboxContent>
-          </Combobox>
+            <Button variant="link" onClick={pushToDashboard}>
+              {`Pick master later`}
+            </Button>
+          </div>
 
           <Button
-            disabled={selectedProgram === null || selectedYear === null}
-            onClick={handleGetStarted}
+            disabled={
+              selectedProgram === null ||
+              selectedYear === null ||
+              selectedMaster === null
+            }
+            onClick={pushToGuide}
             className="w-80 h-12 text-base md:text-lg"
           >
             Get started
+          </Button>
+
+          <Button
+            variant="link"
+            onClick={() => {}}
+            className="text-sm"
+          >
+            Learn more about the project
           </Button>
         </div>
       </main>
@@ -149,3 +198,21 @@ const LandingClientPage = ({ programs }: LandingClientPageProps) => {
   );
 };
 export default LandingClientPage;
+
+const Header = () => {
+  return (
+    <header className="w-full py-6 px-4 flex flex-col items-center">
+      <h1
+        className="text-2xl md:text-6xl font-bold"
+        style={{ fontFamily: '"DM Serif Text", serif' }}
+      >
+        LiU Master
+      </h1>
+      <p className="mb-8 max-w-xl text-center text-lg text-muted-foreground">
+        Drop the crazy spreed sheets.
+        <br />
+        Embrace effortless course planning.
+      </p>
+    </header>
+  );
+};
