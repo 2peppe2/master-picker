2e92fde3a151a96b9a0124d28176264489205677
BakuPlayz <sweplayersuknow@gmail.com>
2026-02-13 23:17:53 +0100

    Fix to infinitely being able to add the same course to wildcard blocks

--- app/dashboard/(schedule)/(block)/GhostBlock.tsx
+++ app/dashboard/(schedule)/(block)/GhostBlock.tsx
@@ -1,37 +1,20 @@
 import { Droppable } from "@/components/Droppable";
 import { Plus } from "lucide-react";
+import { BlockProps } from ".";
 import { FC } from "react";
 
-interface GhostBlockProps {
-  semesterNumber: number;
-  periodNumber: number;
-  blockNumber: number;
-}
-
-const GhostBlock: FC<GhostBlockProps> = ({
-  semesterNumber,
-  periodNumber,
-  blockNumber,
-}) => {
-  const blockId = `ghost-semester-${semesterNumber}-period${periodNumber}-block-${blockNumber}`;
-
-  return (
-    <Droppable
-      id={blockId}
-      data={{
-        semesterNumber,
-        periodNumber,
-        blockNumber,
-      }}
-    >
-      <div className="flex h-40 w-40 items-center justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Plus className="h-6 w-6" />
-          <span className="text-sm font-medium">New Block</span>
-        </div>
+const GhostBlock: FC<BlockProps> = ({ data }) => (
+  <Droppable
+    id={`ghost-semester-${data.semesterNumber}-period${data.periodNumber}-block-${data.blockNumber}`}
+    data={data}
+  >
+    <div className="flex h-40 w-40 items-center justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Plus className="h-6 w-6" />
+        <span className="text-sm font-medium">New Block</span>
       </div>
-    </Droppable>
-  );
-};
+    </div>
+  </Droppable>
+);
 
 export default GhostBlock;
--- app/dashboard/(schedule)/(block)/StandardBlock.tsx
+++ app/dashboard/(schedule)/(block)/StandardBlock.tsx
@@ -1,38 +1,50 @@
+import { useFilterStore } from "@/app/atoms/filter/filterStore";
 import { Draggable } from "@/components/DndProvider/Draggable";
+import { scheduleAtoms } from "@/app/atoms/schedule/atoms";
+import { SemesterOption } from "@/app/atoms/filter/types";
 import { Droppable } from "@/components/Droppable";
 import CourseCard from "@/components/CourseCard";
 import { SearchIcon } from "lucide-react";
-import { BlockViewProps } from "./Block";
+import { useAtomValue } from "jotai";
+import { BlockProps } from ".";
 import { FC } from "react";
 
-const StandardBlock: FC<BlockViewProps> = ({
-  courseSlot,
-  data,
-  blockId,
-  onFilterChange,
-  displayNumber,
-}) => {
-  if (courseSlot) {
-    return (
-      <Droppable data={data} id={blockId}>
+const StandardBlock: FC<BlockProps> = ({ courseSlot, data }) => {
+  const draggedCourse = useAtomValue(scheduleAtoms.draggedCourseAtom);
+
+  const isAlreadyOccupiedByCourse = draggedCourse?.code !== courseSlot?.code;
+  const hasCourseInBlock = courseSlot && isAlreadyOccupiedByCourse;
+
+  const {
+    mutators: { selectBlocks, selectPeriods, selectSemester },
+  } = useFilterStore();
+
+  const handleFilterChange = () => {
+    selectSemester((data.semesterNumber + 1) as SemesterOption);
+    selectPeriods([data.periodNumber + 1]);
+    selectBlocks([data.blockNumber + 1]);
+  };
+
+  return (
+    <Droppable
+      data={data}
+      id={`block-${data.blockNumber}-${data.periodNumber}-${data.blockNumber}`}
+    >
+      {hasCourseInBlock ? (
         <Draggable id={courseSlot.code} data={courseSlot}>
           <CourseCard variant="dropped" course={courseSlot} />
         </Draggable>
-      </Droppable>
-    );
-  }
-
-  return (
-    <Droppable data={data} id={blockId}>
-      <div
-        className="flex flex-col items-center justify-center h-full w-full group cursor-pointer"
-        onClick={onFilterChange}
-      >
-        <SearchIcon className="text-zinc-500 group-hover:text-foreground size-8 mb-2 transition-colors" />
-        <span className="text-zinc-500 font-medium select-none">
-          Block {displayNumber}
-        </span>
-      </div>
+      ) : (
+        <div
+          onClick={handleFilterChange}
+          className="flex flex-col items-center justify-center h-full w-full group cursor-pointer"
+        >
+          <SearchIcon className="text-zinc-500 group-hover:text-foreground size-8 mb-2 transition-colors" />
+          <span className="text-zinc-500 font-medium select-none">
+            Block {data.blockNumber + 1}
+          </span>
+        </div>
+      )}
     </Droppable>
   );
 };
