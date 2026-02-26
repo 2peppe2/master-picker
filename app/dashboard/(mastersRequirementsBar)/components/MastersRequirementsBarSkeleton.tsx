b167e5486e6eb83d92cb4d9f318242b08d1cd758
BakuPlayz <sweplayersuknow@gmail.com>
2026-02-26 23:14:12 +0100

    Improve master requirements bar loading skeleton

--- app/dashboard/(mastersRequirementsBar)/components/MastersRequirementsBarSkeleton.tsx
+++ app/dashboard/(mastersRequirementsBar)/components/MastersRequirementsBarSkeleton.tsx
@@ -1,12 +1,37 @@
-const MastersRequirementsSkeleton = () => (
-  <div className="flex items-center gap-4 w-full animate-pulse">
-    <div className="h-8 w-24 bg-muted rounded shrink-0" />
-    <div className="flex gap-3 overflow-hidden">
-      {[1, 2, 3, 4, 5].map((i) => (
-        <div key={i} className="h-8 w-20 bg-muted rounded-full shrink-0" />
-      ))}
+import { FC, useMemo } from "react";
+import _ from "lodash";
+
+const PROGRAM_AMOUNT_OF_MASTERS: Record<string, number> = {
+  "6CMJU": 7,
+  "6CITE": 8,
+  "6CDDD": 12,
+};
+
+interface MastersRequirementsSkeletonProps {
+  program?: string;
+}
+
+const MastersRequirementsSkeleton: FC<MastersRequirementsSkeletonProps> = ({
+  program,
+}) => {
+  const amountOfMasters = useMemo(() => {
+    if (!program) {
+      return [1, 2, 3, 4, 5];
+    }
+
+    return _.range(PROGRAM_AMOUNT_OF_MASTERS[program]);
+  }, [program]);
+
+  return (
+    <div className="flex items-center w-full h-full gap-4 animate-pulse">
+      <div className="h-8 w-24 bg-muted rounded shrink-0" />
+      <div className="flex gap-3 overflow-hidden w-full">
+        {amountOfMasters.map((i) => (
+          <div key={i} className="h-8 w-full flex-1 bg-muted rounded-full" />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MastersRequirementsSkeleton;
