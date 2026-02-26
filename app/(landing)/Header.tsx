ea6d008a3d12ebec691be39344b00b28d1842cef
lukasabbe <lukasabbehb@gmail.com>
2026-02-26 12:51:03 +0100

    Start work on language selector

--- app/(landing)/Header.tsx
+++ app/(landing)/Header.tsx
@@ -1,26 +1,35 @@
+'use client';
+
 import Image from "next/image";
 import { Playfair_Display } from "next/font/google";
+import { useTranslation } from "react-i18next";
+import '@/lib/i18n';
+
 
 const playfair = Playfair_Display({ weight: "600", subsets: ["latin"] });
 
-const Header = () => (
-  <header className="w-full py-6 px-4 flex flex-col items-center">
-    <div className="flex items-center justify-center gap-4 mb-4">
-      <Image
-        src="/logo/mp_logo_icon.svg"
-        alt="LiU Master Logo"
-        width={70}
-        height={70}
-      />
-      <h1 className={`text-2xl md:text-7xl font-bold ${playfair.className}`}>
-        Master Picker
-      </h1>
-    </div>
+const Header = () => {
+  const { t } = useTranslation("common");
 
-    <p className="mb-8 max-w-xl text-center text-lg text-muted-foreground">
-      Drop the crazy spreed sheets, embrace effortless course planning.
-    </p>
-  </header>
-);
+  return (
+    <header className="w-full py-6 px-4 flex flex-col items-center">
+      <div className="flex items-center justify-center gap-4 mb-4">
+        <Image
+          src="/logo/mp_logo_icon.svg"
+          alt="LiU Master Logo"
+          width={70}
+          height={70}
+        />
+        <h1 className={`text-2xl md:text-7xl font-bold ${playfair.className}`}>
+          {t("headerTitle")}
+        </h1>
+      </div>
+
+      <p className="mb-8 max-w-xl text-center text-lg text-muted-foreground">
+        {t("headerSubtitle")}
+      </p>
+    </header>
+  );
+};
 
 export default Header;
