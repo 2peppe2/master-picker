b2bbe0c854ab024c8ab7251380cc2ba671a14de1
Petrus Jarl <petrus@jarl.nu>
2026-03-04 21:48:41 +0100

    Add department shorthand

---
+++ lib/departmentShortName.ts
@@ -0,0 +1,12 @@
+export const LIU_DEPARTMENTS: Record<string, string> = {
+  "Institutionen för teknik och naturvetenskap": "ITN",
+  "Institutionen för medicinsk teknik": "IMT",
+  "Institutionen för Tema": "TEMA",
+  "Institutionen för fysik, kemi och biologi": "IFM",
+  "Matematiska institutionen": "MAI",
+  "Institutionen för ekonomisk och industriell utveckling": "IEI",
+  "Institutionen för systemteknik": "ISY",
+  "Institutionen för datavetenskap": "IDA",
+  "Institutionen för kultur och samhälle": "IKOS",
+  "Tekniska fakultetens kansli": "TFK"
+};
