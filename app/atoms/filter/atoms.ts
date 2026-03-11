6aaeb3fbbb05d98aec44e222b6469cee93571231
Mikael Karlsson <sweplayersuknow@gmail.com>
2026-03-11 12:24:22 +0100

    Add default for semester 7

--- app/atoms/filter/atoms.ts
+++ app/atoms/filter/atoms.ts
@@ -3,7 +3,7 @@ import { atomWithReset } from "jotai/utils";
 export const filterAtoms = {
   searchAtom: atomWithReset<string>(""),
   mastersAtom: atomWithReset<string[]>([]),
-  semestersAtom: atomWithReset<number[]>([]),
+  semestersAtom: atomWithReset<number[]>([7]),
   periodsAtom: atomWithReset<number[]>([]),
   blocksAtom: atomWithReset<number[]>([]),
   excludeSlotConflictsAtom: atomWithReset<boolean>(false),
