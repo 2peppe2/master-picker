e967dfd2770ee7e53d94e2f471f33ae380f5f860
Lukas <67807954+lukasabbe@users.noreply.github.com>
2026-03-14 10:48:52 +0100

    Fix evaluate score graph and now using package (#193)

--- components/CourseModal/courseDataCache.ts
+++ components/CourseModal/courseDataCache.ts
@@ -1,43 +1,12 @@
-export type Grade = {
-  grade: string;
-  gradeOrder: number;
-  quantity: number;
-};
+import { DataCourseStatistic, getCourseStatistic } from "liu-tentor-package"
+export const fetchPromiseCache = new Map<string, DataCourseStatistic>();
 
-export type Module = {
-  moduleCode: string;
-  date: string;
-  grades: Grade[];
-};
-
-export type EvaluationReport = {
-  reportId: number;
-  reportDate: string;
-  scores: Record<string, number>;
-};
-
-export type CourseData = {
-  courseCode: string;
-  courseNameSwe: string;
-  courseNameEng: string;
-  lastUpdatedTimestamp: string;
-  modules: Module[];
-  evaluationReports?: EvaluationReport[];
-};
-
-export const fetchPromiseCache = new Map<string, Promise<CourseData>>();
-
-export async function fetchCourseData(courseCode: string): Promise<CourseData> {
+export async function fetchCourseData(courseCode: string): Promise<DataCourseStatistic> {
   if (!fetchPromiseCache.has(courseCode)) {
-    const request = fetch(
-      `https://liutentor.lukasabbe.com/api/courses/${courseCode}`
-    ).then((res) => {
-      if (!res.ok) throw new Error("Failed to fetch course data");
-      return res.json();
-    });
+    const course = await getCourseStatistic(courseCode) as DataCourseStatistic;
 
-    fetchPromiseCache.set(courseCode, request);
+    fetchPromiseCache.set(courseCode, course);
   }
 
-  return fetchPromiseCache.get(courseCode) as Promise<CourseData>;
+  return fetchPromiseCache.get(courseCode) as DataCourseStatistic;
 }
